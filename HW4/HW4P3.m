9c610e8eabaff82ba5f33fa9c21ce11f5c72b701
Gerald LaMountain <lamountain.g@husky.neu.edu>
2018-04-18 14:27:15 -0400

    Add HW4P3.m

---
+++ HW4/HW4P3.m
@@ -0,0 +1,29 @@
+clearvars
+close all
+clc
+
+% Load hidden code
+load('HW4P3.mat');
+[~,len1] = size(ca_code_hidden);
+
+% % Load all codes
+% load('Cacodes.mat');
+
+% Generate codes
+ca_code_matrix = CAcodegen([1:32]);
+
+[nc,len2] = size(ca_code_matrix);
+
+xcor_mat = zeros(nc,len1+len2-1);
+for i = 1:size(ca_code_matrix,1)
+    xcor_mat(i,:) = xcorr(ca_code_hidden, ca_code_matrix(i,:));
+end
+
+% Find maximum
+[~,sat] = max(max(xcor_mat,[],2));
+
+% Plot cross correlations
+plot(0:len1+len2-2,xcor_mat)
+title(sprintf('Hidden code cross correlation with known C/A Codes, Max=Sat#%d',sat))
+xlabel('Lag')
+ylabel('Cross Correlation');
