dd2c384cd5b57216438a50fac8a482f841f74b86
Gerald LaMountain <gerald.lamountain@outlook.com>
2018-04-19 15:56:01 -0400

    HW4P2 Code and figures

--- HW4/EECE5698_CAcode.m
+++ HW4/EECE5698_CAcode.m
@@ -1,7 +1,7 @@
 %   EECE5698-ST: GNSS signal processing
 %       GPS L1 C/A code generation
 %
-%   Pau Closas, Spring 2018
+%   Pau Closas, Gerald LaMountain, Spring 2018
 
 clearvars
 close all
@@ -18,22 +18,36 @@ Tc = 1/Rc;
 [ca_code]=CAcodegen(svnum);
 [ca_code2]=CAcodegen(svnum+1);
 
-figure;
+fig1 = figure;
 subplot(2,1,1); plot(ca_code);
+title(sprintf('C/A Code for Satellite Vehicle #%d',svnum));
 subplot(2,1,2); plot(ca_code2);
+title(sprintf('C/A Code for Satellite Vehicle #%d',svnum+1));
+saveas(fig1,['.\latex\figures\HW4P2a.png'])
+
 
 %% Compute and plot autocorrelation(s)
 ca_acor_line = xcorr (ca_code,ca_code);
 ca_acor_circ = xcorr_circ(ca_code,ca_code);
 
-figure;
+fig2 = figure;
 subplot(2,1,1); plot(ca_acor_line);
+title(sprintf('Linear Auto-Correlation of C/A Code for Satellite Vehicle #%d',svnum));
+xlabel('Lag');
 subplot(2,1,2); plot(ca_acor_circ);
+title(sprintf('Circular Auto-Correlation of C/A Code for Satellite Vehicle #%d',svnum));
+xlabel('Lag');
+saveas(fig2,['.\latex\figures\HW4P2b.png'])
+
 
 
 %% Compute and plot crosscorrelation with concatenated replicas
 ca_ccor_line_cat = xcorr (ca_code,[ca_code ca_code ca_code]);
 figure; plot(ca_ccor_line_cat);
+title('Cross-Correlation between ca_code and thrice replicated c_code');
+xlabel('Lag');
+saveas(fig2,['.\latex\figures\HW4P2c.png'])
+
 
 %% Compute and plot crosscorrelation(s)
 ca_xcor_line = xcorr (ca_code,ca_code2);
@@ -41,4 +55,7 @@ ca_xcor_circ = xcorr_circ(ca_code,ca_code2);
 
 figure;
 subplot(2,1,1); plot(ca_xcor_line);
+title(sprintf('Linear Cross-Correlation between C/A Code for Satellite Vehicle #%d and #%d',svnum,svnum+1));
 subplot(2,1,2); plot(ca_xcor_circ);
+title(sprintf('Circular Cross-Correlation between C/A Code for Satellite Vehicle #%d and #%d',svnum,svnum+1));
+saveas(fig2,['.\latex\figures\HW4P2d.png'])
