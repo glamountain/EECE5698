627a17d49aa9b2c278485342d57955d2764c409d
Gerald LaMountain <gerald.lamountain@outlook.com>
2018-04-19 16:29:43 -0400

    Updated HW4P2 figures

--- HW4/EECE5698_CAcode.m
+++ HW4/EECE5698_CAcode.m
@@ -19,9 +19,9 @@ Tc = 1/Rc;
 [ca_code2]=CAcodegen(svnum+1);
 
 fig1 = figure;
-subplot(2,1,1); plot(ca_code);
+subplot(2,1,1); stem(ca_code);
 title(sprintf('C/A Code for Satellite Vehicle #%d',svnum));
-subplot(2,1,2); plot(ca_code2);
+subplot(2,1,2); stem(ca_code2);
 title(sprintf('C/A Code for Satellite Vehicle #%d',svnum+1));
 saveas(fig1,['.\latex\figures\HW4P2a.png'])
 
@@ -31,10 +31,10 @@ saveas(fig1,['.\latex\figures\HW4P2a.png'])
 [ca_acor_circ, lc] = xcorr_circ(ca_code,ca_code);
 
 fig2 = figure;
-subplot(2,1,1); plot(ll, ca_acor_line);
+subplot(2,1,1); stem(ll, ca_acor_line);
 title(sprintf('Linear Auto-Correlation of C/A Code for Satellite Vehicle #%d',svnum));
 xlabel('Lag');
-subplot(2,1,2); plot(lc, ca_acor_circ);
+subplot(2,1,2); stem(lc, ca_acor_circ);
 title(sprintf('Circular Auto-Correlation of C/A Code for Satellite Vehicle #%d',svnum));
 xlabel('Lag');
 saveas(fig2,['.\latex\figures\HW4P2b.png'])
@@ -43,7 +43,7 @@ saveas(fig2,['.\latex\figures\HW4P2b.png'])
 
 %% Compute and plot crosscorrelation with concatenated replicas
 [ca_ccor_line_cat, ll] = xcorr (ca_code,[ca_code ca_code ca_code]);
-fig3 = figure; plot(ll, ca_ccor_line_cat);
+fig3 = figure; stem(ll, ca_ccor_line_cat);
 title('Cross-Correlation between ca_code and thrice replicated c_code');
 xlabel('Lag');
 saveas(fig3,['.\latex\figures\HW4P2c.png'])
@@ -54,8 +54,8 @@ saveas(fig3,['.\latex\figures\HW4P2c.png'])
 [ca_xcor_circ, lc] = xcorr_circ(ca_code,ca_code2);
 
 fig4 = figure;
-subplot(2,1,1); plot(ll, ca_xcor_line);
+subplot(2,1,1); stem(ll, ca_xcor_line);
 title(sprintf('Linear Cross-Correlation between C/A Code for Satellite Vehicle #%d and #%d',svnum,svnum+1));
-subplot(2,1,2); plot(lc, ca_xcor_circ);
+subplot(2,1,2); stem(lc, ca_xcor_circ);
 title(sprintf('Circular Cross-Correlation between C/A Code for Satellite Vehicle #%d and #%d',svnum,svnum+1));
 saveas(fig4,['.\latex\figures\HW4P2d.png'])
